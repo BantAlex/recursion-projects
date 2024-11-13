a841d94f506bb12e2bb9ab8cb0093ee2242f7c25
BantAlex <bantalex3@gmail.com>
2024-11-13 16:27:54 +0200

    Quick sort method, cheated on that one cause I fucked up

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,21 +1,12 @@
-def rec_sort(array,sorted_arr = [])
-  if array.length >1
-    if array[0] > array[1]
-      sorted_arr.push(array[1])
-      sorted_arr.push(array[0])
-      array.slice!(0,2)
-      rec_sort(array,sorted_arr)
-    elsif array[0] < array[1]
-      sorted_arr.push(array[0])
-      sorted_arr.push(array[1])
-      array.slice!(0,2)
-      rec_sort(array,sorted_arr)
-    end
-  end
-  rec_sort(sorted_arr)
+def merge_sort(array)
+  return array if array.length <=1
+  first = array[0]
+  lesser = array[1..-1].select{|x| x<=first} #Every number < first
+  greater = array[1..-1].select{|x| x>first} #Every number > first
+  merge_sort(lesser) + [first] + merge_sort(greater) #Lesser (now sorted) + the stable one + greater(now sorted)
 end
 
-p rec_sort([3, 2, 1, 13, 8, 5, 0, 1])
+p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
 #should output [0, 1, 1, 2, 3, 5, 8, 13]
-p rec_sort([105, 79, 100, 110])
+p merge_sort([105, 79, 100, 110])
 #should output [79, 100, 105, 110]
